5f4a7dfed0fcfdc6c70bf2479bee60448ca1ad62
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-04-27 00:32:11 +0200

    First Commit

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.kalkulator;
+
+public class Calculator {
+
+    static void add(int a, int b){
+        System.out.println(a+"+"+b+"="+(a+b));
+    }
+    static void subtract(int a, int b){
+        System.out.println(a+"-"+b+"="+(a-b));
+    }
+
+    public static void main(String[] args) {
+        add(5,2);
+        subtract(5,2);
+    }
+}
